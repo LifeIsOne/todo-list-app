c2944c8255577e5dbe3a59e8c6360908156df92c
LifeIsOne <didtmdgh258@gmail.com>
2025-05-21 01:59:23 +0900

    [REFACTORING] 컴포넌트, 레이아웃, 홈 Vue로 전환

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).mount('#app')
---
+++ resources/js/components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Todo App</a>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
