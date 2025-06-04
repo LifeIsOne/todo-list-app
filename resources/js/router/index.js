7c2b51ca606f16d5f07af6010ddc31f4fc0fb935
LifeIsOne <didtmdgh258@gmail.com>
2025-06-05 01:46:31 +0900

    [FEAT] 상세 페이지

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -11,7 +11,7 @@ const routes = [
         component: HomeView,
     },
     {
-        path: '/detail/:id',
+        path: '/todos/:id',
         name: 'Detail',
         component: DetailView,
         props: true,
--- resources/js/views/EditView.vue
+++ resources/js/views/EditView.vue
@@ -1,7 +1,35 @@
 <template>
     <div class="container py-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center my-5 border-bottom">
-            <h1>EditView</h1>
+            <h2>수정 페이지</h2>
+            <div class="d-flex gap-2">
+                <button class="btn btn-danger" @click="deleteTodo">
+                    수정완료
+                </button>
+            </div>
+        </div>
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="badge" :class="todo.completed ? 'bg-success' : 'bg-warning'">
+                        {{ todo.completed ? '완료' : '진행중' }}
+                    </div>
+                </div>
+
+                <p class="card-text fs-5 mb-4">{{ todo.text }}</p>
+
+                <div class="d-flex justify-content-between text-muted">
+                    <small>
+                        <i class="bi bi-calendar me-1"></i>
+                        생성일: {{ (todo.createdAt) }}
+                    </small>
+                    <small>
+                        <!-- TODO: 완료일 추가 -->
+                        <i class="bi bi-check-circle me-1"></i>
+                        완료일: {{ todo.completed ?  todo.createdAt  : '미완료' }}
+                    </small>
+                </div>
+            </div>
         </div>
     </div>
 </template>
