282a7a2cef9d0b8a868c807ccdb631d316c7f4b5
LifeIsOne <didtmdgh258@gmail.com>
2025-06-07 17:22:33 +0900

    [FIX] text 유효성 검사, 얼럿 추가

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,78 +3,105 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
+use Exception;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    // 홈, 할 일 목록
-//    public function home() {
-//        $todos = Todo::all();
-//
-//        return view('home', ['todos' => $todos]);
-//    }
-    public function home(Request $request) {
-        $filter = $request->query('filter');
-        $todos = Todo::when($filter === 'completed', function ($query) {$query->where('completed', true);})
-                     ->when($filter === 'incomplete', function ($query) {$query->where('completed', false);})
-                     ->get();
+    // 메인, 할 일 리스트
+    public function home(Request $req)
+    {
+        $filter = $req->query('filter');
+        $todos = Todo::when($filter === 'completed', function ($query) {
+            $query->where('completed', true);
+        })
+            ->when($filter === 'incomplete', function ($query) {
+                $query->where('completed', false);
+            })
+            ->get();
 
         return view('home', ['todos' => $todos, 'filter' => $filter]);
     }
 
-    // 상세보기 페이지
-    public function detail($id) {
+    // 상세보기
+    public function detail($id)
+    {
         $todo = Todo::find($id);
         return view('todo.detail', ['todo' => $todo]);
     }
 
     // 추가, 할 일 추가
-    public function create() {
+    public function create()
+    {
         return view('todo.edit');
     }
 
     // 저장 요청
-    public function store(Request $request) {
+    public function store(Request $req)
+    {
         // 유효성 검사 `text` 필수, 50자 내, 유일
-        $request->validate([
-            'text' => 'required|string|max:50|unique:todos,text',
-        ]);
-        // DB에 저장
-        Todo::create([
-            'text' => $request->text,
-        ]);
-        // 리다이렉션
-        return redirect('/');
+        try {
+            $req->validate([
+                'text' => 'required|string|min:2|max:50|unique:todos,text'], [
+                'text.required' => '내용을 입력해주세요!',
+                'text.min' => '2자 이상 50자 이하여야 합니다!',
+                'text.max' => '2자 이상 50자 이하여야 합니다!',
+                'text.unique' => '중복된 내용입니다!',
+            ]);
+
+            Todo::create([
+                'text' => $req->text,
+                'completed' => false,
+            ]);
+            return redirect('/');
+        } catch (Exception $e) {
+            return redirect()->back()
+                ->with('error', $e->getMessage());
+        }
     }
 
     // 수정, 할 일 수정
-    public function edit($id) {
+    public function edit($id)
+    {
         $todo = Todo::find($id);
 
         return view('todo.edit', ['todo' => $todo]);
     }
 
 
-    public function update(Request $request, $id) {
-        $request->validate([
-            'text' => 'required',
-        ]);
+    public function update(Request $req, $id)
+    {
+        try {
+            $req->validate([
+                'text' => 'required|string|min:2|max:50|unique:todos,text'], [
+                'text.required' => '내용을 입력해주세요!',
+                'text.min' => '2자 이상 50자 이하여야 합니다!',
+                'text.max' => '2자 이상 50자 이하여야 합니다!',
+                'text.unique' => '중복된 내용입니다!',
+            ]);
 
-        $todo = Todo::find($id);
-        $todo->text = $request->text;
-        $todo->save();
+            $todo = Todo::find($id);
+            $todo->text = $req->text;
+            $todo->save();
 
-        return redirect('/');
+            return redirect('/');
+        } catch (Exception $e) {
+            return redirect()->back()
+                ->with('error', $e->getMessage())
+                ->withInput();
+        }
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         $todo = Todo::find($id);
         $todo->delete();
 
         return redirect('/');
     }
 
-    public function complete($id){
+    public function complete($id)
+    {
         $todo = Todo::find($id);
         $todo->completed = !$todo->completed;
         $todo->save();
